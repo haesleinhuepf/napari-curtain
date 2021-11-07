2161a085fae35f9371ea3729340f50cfa9f519f1
Robert Haase <haesleinhuepf@users.noreply.github.com>
2021-11-07 17:51:12 +0100

    make sure it's in the menus

--- src/napari_curtain/_function.py
+++ src/napari_curtain/_function.py
@@ -1,26 +1,18 @@
 from napari_plugin_engine import napari_hook_implementation
 import napari
-from magicgui import magicgui
+from magicgui import magicgui, magic_factory
 import numpy as np
-from napari_tools_menu import register_action
+from napari_tools_menu import register_dock_widget
 
 # This is the actual plugin function, where we export our function
 # (The functions themselves are defined below)
 @napari_hook_implementation
-def napari_experimental_provide_function():
-    return [curtain_gpu]
+def napari_experimental_provide_dock_widget():
+    return [curtain]
 
 
-@register_action(menu="Visualization > Curtain")
-def start_curtain(viewer):
-    viewer.window.add_dock_widget(curtain, name="Curtain")
-
-
-# We use magicgui to make the position a slider. Furthermore,
-# we make the function be called once the user changes a
-# parameter. Read more about magicgui:
-# https://napari.org/magicgui/usage/configuration.html
-@magicgui(
+@register_dock_widget(menu="Visualization > Curtain")
+@magic_factory(
     auto_call=True,
     position={'widget_type': 'Slider', 'min': 0, 'max': 100}
 )
@@ -31,15 +23,15 @@ def curtain(
     factor2: float = 1,
     position: float = 50
 ) -> napari.types.ImageData:
-    copy = image1 * factor1
-
     if image1 is None or image2 is None:
         return
+
     # check if image shapes are equal
     assert np.array_equal(image1.shape, image2.shape), "Image dimensions should match"
-
+    # make sure it's numpy and not dask, clesperanto or any other array
     image1 = np.asarray(image1)
     image2 = np.asarray(image2)
+    copy = image1 * factor1
 
     curtain_position = int(position / 100 * image1.shape[-1])
     copy[..., curtain_position:] = image2[..., curtain_position:] * factor2
